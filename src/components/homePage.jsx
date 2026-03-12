0d1a80219c0d38d3cd84b5243b253dbe9d6b2fe1
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-03-12 16:03:22 +0100

    update

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,5 +1,7 @@
 import { CardSlider } from './cardSlider';
 import { Transaction } from './transaction';
+import { MyBarChart } from './barChart';
+import MyPieChart from './pieChart';
 import { NavLink } from 'react-router-dom';
 export const HomePage = () => {
   return (
@@ -25,20 +27,20 @@ export const HomePage = () => {
           <Transaction />
         </div>
       </div>
-      {/* <div className="flex flex-col  md:flex-row md:gap-x-8 md:items-center md:mt-6">
-        <div className="min-w-0 md:flex-2 ">
-          <div className="flex justify-between md:mb-5 main-color font-semibold inter-font">
+      <div className="flex flex-col mt-4  gap-y-4 md:flex-row md:gap-x-8 md:items-center md:mt-6">
+        <div className="min-w-0 md:flex-2  ">
+          <div className="flex justify-between mb-4 md:mb-5 main-color font-semibold inter-font">
             <h1 className="text-[18px] ">Weekly Activity</h1>
           </div>
-          <Transaction />
+          <MyBarChart />
         </div>
         <div className="min-w-0 md:flex-1">
-          <div className="md:mb-5 main-color font-semibold inter-font text-[18px]">
+          <div className="md:mb-5 mb-4 main-color font-semibold inter-font text-[18px]">
             Expense Statistics
           </div>
-          <Transaction />
+          <MyPieChart />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
